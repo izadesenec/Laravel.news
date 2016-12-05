33a2e83c82d1eb9f114aab3fbbefdd524dbc03bb
max <max@max>
2016-12-05 20:47:38 +0200

    cooo

---
+++ blog/app/Http/routes.php
@@ -0,0 +1,58 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+
+  use App\Task;
+  use Illuminate\Http\Request;
+
+  /**
+   * Вывести список всех задач
+   */
+ Route::get('/', function () {
+    $tasks = Task::orderBy('created_at', 'asc')->get();
+    $tasks = Task::where('id','=','1')->get();
+
+  return view('tasks', [
+    'tasks' => $tasks
+  ]);
+});
+
+  /**
+   * Добавить новую задачу
+   */
+Route::post('/task', function (Request $request) {
+  $validator = Validator::make($request->all(), [
+    'name' => 'required|max:255',
+    'description'=>'required|max:255',  
+  ]);
+
+  if ($validator->fails()) {
+    return redirect('/')
+      ->withInput()
+      ->withErrors($validator);
+  }
+
+  $task = new Task;
+  $task->name = $request->name;
+  $task->description=$request->description;
+  $task->save();
+
+  return redirect('/');
+});
+
+  /**
+   * Удалить существующую задачу
+   */
+  Route::delete('/task/{id}', function ($id) {
+    //
+  });
---
+++ blog/resources/views/Tasks.blade.php
@@ -0,0 +1,82 @@
+@extends('layouts.app')
+
+@section('content')
+
+  <!-- Bootstrap шаблон... -->
+
+  <div class="panel-body">
+    <!-- Отображение ошибок проверки ввода -->
+   @include('common.errors')
+
+    <!-- Форма новой задачи -->
+    <form action="/task" method="POST" class="form-horizontal">
+      {{ csrf_field() }}
+
+      <!-- Имя задачи -->
+      <div class="form-group">
+        <label for="task" class="col-sm-3 control-label">Задача</label>
+
+        <div class="col-sm-6">
+          <input type="text" name="name" id="task-name" class="form-control">
+        </div>
+      </div>
+      <label for="description" class="col-sm-3 control-label">Описание</label>
+
+        <div class="col-sm-6">
+          <input type="text" name="description" id="task-description" class="form-control">
+        </div>
+
+      <!-- Кнопка добавления задачи -->
+      <div class="form-group">
+        <div class="col-sm-offset-3 col-sm-6">
+          <button type="submit" class="btn btn-default">
+            <i class="fa fa-plus"></i> Добавить задачу
+          </button>
+        </div>
+      </div>
+    </form>
+  </div>
+
+  <!-- Текущие задачи -->
+
+@if (count($tasks) > 0)
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        Текущая задача
+      </div>
+
+      <div class="panel-body">
+        <table class="table table-striped task-table">
+
+          <!-- Заголовок таблицы -->
+          <thead>
+            <th>Task</th>
+            <th>&nbsp;</th>
+          </thead>
+
+          <!-- Тело таблицы -->
+          <tbody>
+            @foreach ($tasks as $task)
+              <tr>
+                <!-- Имя задачи -->
+                <td class="table-text">
+                    <div>{{ $task->name }}<br>{{$task->description}}</div>
+                </td>
+
+                <td>
+                  <!--Кнопка Удалить -->
+                  <form action="/task/{{ $task->id }}" method="POST">
+      {{ csrf_field() }}
+      {{ method_field('DELETE') }}
+
+      <button class="btn btn-danger">Удалить задачу</button>
+    </form>
+                </td>
+              </tr>
+            @endforeach
+          </tbody>
+        </table>
+      </div>
+    </div>
+   @endif
+   @endsection
